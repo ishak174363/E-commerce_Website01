d9260993772b66fabb61404ddc231f763278c3d0
Ishak Sheikh <you@example.com>
2023-06-23 19:53:20 +0600

    Add to Cart

--- ecomsite/shop/views.py
+++ ecomsite/shop/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.core.paginator import Paginator
-from .models import Products
+from .models import Products,Order
 # Create your views here.
 
 def index(request):
@@ -25,4 +25,17 @@ def detail(request,id):
 
 
 def checkout(request):
+
+    if request.method=="POST":
+        items=request.POST.get('items','')
+        name=request.POST.get('name',"")
+        email=request.POST.get('email',"")
+        address=request.POST.get('address',"")
+        city=request.POST.get('city',"")
+        state=request.POST.get('state',"")
+        zipcode=request.POST.get('zipcode',"")
+        total=request.POST.get('total',"")
+        order=Order(items=items,name=name,email=email,address=address,city=city,state=state,zipcode=zipcode,total=total)
+        order.save()   
+
     return render(request,'shop/checkout.html')
