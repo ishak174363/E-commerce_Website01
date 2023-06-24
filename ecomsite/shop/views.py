17d8a3ebbce92822f5e6ca7cfb8443f5c9a2d32c
Ishak Sheikh <you@example.com>
2023-06-25 00:04:17 +0600

    Edited with carousel

--- ecomsite/shop/views.py
+++ ecomsite/shop/views.py
@@ -12,7 +12,7 @@ def index(request):
         product_objects = product_objects.filter(title__icontains=item_name)
 
     #paginator code
-    paginator = Paginator(product_objects,8)
+    paginator = Paginator(product_objects,4)
     page = request.GET.get('page')
     product_objects = paginator.get_page(page)
     
